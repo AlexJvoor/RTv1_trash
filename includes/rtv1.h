94a62d6efa10330e9853cc204c6e423d1f32f822
ale_xxx <vasyekk67@yandex.ru>
2020-10-05 14:53:19 +0300

    extra in .h

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -5,20 +5,31 @@
 #include "mlx.h"
 #include "libnum.h"
 
+typedef enum	e_type
+{
+	SPHERE,
+	PLANE,
+	CONE,
+	CYLINDER
+}				t_type;
+
 typedef struct  s_plane
 {
+	int8_t		type;
     t_vec3      coords;//произв точка на пл-ти
     float       dist;//кратчайшее расстояние до плоскости
 }				t_plane;
 
 typedef struct  s_sphere
 {
+	int8_t		type;
 	t_vec3      coords;//центр
 	float       rad;
 }				t_spere;
 
 typedef struct  s_cone
 {
+	int8_t		type;
 	t_vec3		coords;//центр
 	t_vec3		dir_vec;//вектор направления конуса
 	float		tg;//тангенс от 1/2 угла расширения конуса
@@ -26,6 +37,7 @@ typedef struct  s_cone
 
 typedef struct  s_limcone//огранич. конус
 {
+	int8_t		type;
 	t_vec3		coords;//центр
 	t_vec3		dir_vec;//вектор направления конуса
 	float		tg;//тангенс от 1/2 угла расширения конуса
@@ -35,6 +47,7 @@ typedef struct  s_limcone//огранич. конус
 
 typedef struct	s_cylinder
 {
+	int8_t		type;
 	t_vec3		coords;//произв. точка на оси цилиндра
 	t_vec3		dir_vec;//напр цилиндра
 	float		rad;
@@ -42,6 +55,7 @@ typedef struct	s_cylinder
 
 typedef struct	s_limcylinder
 {
+	int8_t		type;
 	t_vec3		max;//произв. точка на оси цилиндра
 	t_vec3		dir_vec;//напр цилиндра
 	float		rad;
@@ -49,10 +63,17 @@ typedef struct	s_limcylinder
 
 typedef struct	s_light
 {
+	// int8_t		type;
 	t_vec3		coord;
 	float		light_pov;
+	// t_light		*next;
 	//smth else coming
 }				t_light;
+
+typedef struct	s_obj
+{
+	int8_t		type;
+}				t_obj;
 
 typedef struct	s_objs
 {
@@ -64,5 +85,15 @@ typedef struct	s_objs
 	t_limcylinder	*lcylinders;
 	t_light			*lights;
 }				t_objs;
+
+typedef struct	s_data
+{
+	t_light		*light;
+	t_list		*objs;
+	void		*mlx;//mb another struct for mlx
+	void		*win;
+	void		*img;
+	int			*data;
+}				t_data;
 
 # endif
